2b7f5c44aad4daed2013c9f087ec1b2f11d61304
Omarhassan00 <135049722+Omarhassan00@users.noreply.github.com>
2024-01-18 01:01:07 +0200

    Add files via upload

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,272 +1,109 @@
 #!/usr/bin/python3
-"""
-This module uses unittest to test FileStorage class
-"""
-
+""" Module for testing file storage"""
 import unittest
-import os
-import json
-from models.engine.file_storage import FileStorage
-from models import storage
-from datetime import datetime
 from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models import storage
+import os
 
 
-class TestFileStorage(unittest.TestCase):
-    """
-    To run all the possible tests of FileStorage class
-    using unittest
-    """
+class test_fileStorage(unittest.TestCase):
+    """ Class to test the file storage method """
 
     def setUp(self):
-        """ starts a new connection """
-
-        setattr(storage, "_FileStorage__objects", {})
-        pass
+        """ Set up test environment """
+        del_list = []
+        for key in storage._FileStorage__objects.keys():
+            del_list.append(key)
+        for key in del_list:
+            del storage._FileStorage__objects[key]
 
     def tearDown(self):
-        """ Removes all the connections (restoring the initial state)"""
-
-        if os.path.exists('file.json'):
+        """ Remove storage file at end of tests """
+        try:
             os.remove('file.json')
-        pass
+        except:
+            pass
 
-    def test_FileStorage_type(self):
-        """ What is a FileStorage """
+    def test_obj_list_empty(self):
+        """ __objects is initially empty """
+        self.assertEqual(len(storage.all()), 0)
 
-        filestorage_type = "<class 'models.engine.file_storage.FileStorage'>"
-        self.assertEqual(str(FileStorage), filestorage_type)
-
-    def test_FileStorage_attr(self):
-        """ checks if FileStorage has its attributes """
-
-        my_storage = FileStorage()
-        self.assertTrue(hasattr(my_storage, "_FileStorage__file_path"))
-        self.assertTrue(hasattr(my_storage, "_FileStorage__objects"))
-
-    def test_instance_type(self):
-        """ Is an instance a FileStorage """
-
-        my_storage = FileStorage()
-        self.assertIsInstance(my_storage, FileStorage)
-
-    def test_instance_attr_type(self):
-        """ checks the types of the attributes """
-
-        my_storage = FileStorage()
-        my_file = getattr(my_storage, '_FileStorage__file_path')
-        my_objects = getattr(my_storage, '_FileStorage__objects')
-        self.assertEqual(str(type(my_file)), "<class 'str'>")
-        self.assertEqual(str(type(my_objects)), "<class 'dict'>")
-
-    def test_init_0_args(self):
-        """ passing zero arguments (no self) """
-
-        with self.assertRaises(TypeError) as excpt:
-            FileStorage.__init__()
-        excpt_msg = "__init__() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_init_self_arg(self):
-        """ check the initialized values of the attributes """
-
-        my_storage = FileStorage()
-        self.assertEqual(my_storage._FileStorage__file_path, 'file.json')
-        self.assertEqual(my_storage._FileStorage__objects, {})
-
-    def test_init_2_arg(self):
-        """ check the initialized values of the attributes
-        when passing extra args """
-
-        with self.assertRaises(TypeError) as excpt:
-            my_storage = FileStorage('junk')
-        excpt_msg = "__init__() takes 1 positional argument but 2 were given"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_objects_after_basemodel(self):
-        """ checks if a basemodel object is added to objects """
-
-        my_model = BaseModel()
-        key = 'BaseModel.' + my_model.id
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(my_objects[key], my_model)
-
-    def test_objects_len_after_1_obj(self):
-        """ Creating an object, then
-        ckecks if the length of objects is really 1 """
-
-        my_model = BaseModel()
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(len(my_objects), 1)
-
-    def test_objects_len_after_3_obj(self):
-        """ Creating an object, then
-        ckecks if the length of objects is really 1 """
-
-        my_model = BaseModel()
-        my_user = User()
-        my_city = City()
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(len(my_objects), 3)
+    def test_new(self):
+        """ New object is correctly added to __objects """
+        new = BaseModel()
+        for obj in storage.all().values():
+            temp = obj
+        self.assertTrue(temp is obj)
 
     def test_all(self):
-        """ test a normal call to all method """
+        """ __objects is properly returned """
+        new = BaseModel()
+        temp = storage.all()
+        self.assertIsInstance(temp, dict)
 
-        my_model = BaseModel()
-        my_user = User()
-        my_city = City()
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(my_objects, storage.all())
+    def test_base_model_instantiation(self):
+        """ File is not created on BaseModel save """
+        new = BaseModel()
+        self.assertFalse(os.path.exists('file.json'))
 
-    def test_all_without_self(self):
-        """ calling call without self argument """
+    def test_empty(self):
+        """ Data is saved to file """
+        new = BaseModel()
+        thing = new.to_dict()
+        new.save()
+        new2 = BaseModel(**thing)
+        self.assertNotEqual(os.path.getsize('file.json'), 0)
 
-        with self.assertRaises(TypeError) as excpt:
-            FileStorage.all()
-        excpt_msg = "all() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_all_with_args(self):
-        """ calling call with some args """
-
-        with self.assertRaises(TypeError) as excpt:
-            storage.all('junk')
-        excpt_msg = "all() takes 1 positional argument but 2 were given"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_save00(self):
-        """ a normal call to save method with no objects """
-
+    def test_save(self):
+        """ FileStorage save method """
+        new = BaseModel()
         storage.save()
-        with open('file.json', 'r', encoding='utf-8') as f:
-            text_in_file = f.read()
-        self.assertEqual(text_in_file, '{}')
+        self.assertTrue(os.path.exists('file.json'))
 
-    def test_save01(self):
-        """ a normal call to save method with one object """
-
-        my_model = BaseModel()
-        storage.save()
-        with open('file.json', 'r', encoding='utf-8') as f:
-            text_in_file = f.read()
-        my_text = '{"BaseModel.'
-        my_text += my_model.id
-        my_text += '": '
-        my_text += json.dumps(my_model.to_dict())
-        my_text += '}'
-        self.assertEqual(text_in_file, my_text)
-        self.maxDiff = None
-
-    def test_save02(self):
-        """ a normal call to save method with 3 objects """
-
-        my_model = BaseModel()
-        storage.save()
-        my_user = User()
-        my_review = Review()
-        storage.save()
-        with open('file.json', 'r', encoding='utf-8') as f:
-            text_in_file = f.read()
-        my_text = '{'
-        my_text += '"BaseModel.'
-        my_text += my_model.id
-        my_text += '": '
-        my_text += json.dumps(my_model.to_dict())
-        my_text += ', '
-        my_text += '"User.'
-        my_text += my_user.id
-        my_text += '": '
-        my_text += json.dumps(my_user.to_dict())
-        my_text += ', '
-        my_text += '"Review.'
-        my_text += my_review.id
-        my_text += '": '
-        my_text += json.dumps(my_review.to_dict())
-        my_text += '}'
-        self.assertEqual(text_in_file, my_text)
-        self.maxDiff = None
-
-    def test_save_without_self(self):
-        """ calling save without self argument """
-
-        with self.assertRaises(TypeError) as excpt:
-            FileStorage.save()
-        excpt_msg = "save() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_save_with_args(self):
-        """ calling save with some args """
-
-        with self.assertRaises(TypeError) as excpt:
-            storage.save('junk')
-        excpt_msg = "save() takes 1 positional argument but 2 were given"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_reload_without_self(self):
-        """ calling reload without self argument """
-
-        with self.assertRaises(TypeError) as excpt:
-            FileStorage.reload()
-        excpt_msg = "reload() missing 1 required positional argument: 'self'"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_reload_with_args(self):
-        """ calling reload with some args """
-
-        with self.assertRaises(TypeError) as excpt:
-            storage.reload('junk')
-        excpt_msg = "reload() takes 1 positional argument but 2 were given"
-        self.assertEqual(str(excpt.exception), excpt_msg)
-
-    def test_reload_with_no_file(self):
-        """ test the reload method when file.json doesn't exixt """
-
-        storage.reload()
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(my_objects, {})
-
-    def test_reload_no_obj_in_file(self):
-        """ test the reload method when file.json has no objects """
-
+    def test_reload(self):
+        """ Storage file is successfully loaded to __objects """
+        new = BaseModel()
         storage.save()
         storage.reload()
-        my_objects = getattr(storage, '_FileStorage__objects')
-        self.assertEqual(my_objects, {})
+        for obj in storage.all().values():
+            loaded = obj
+        self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
-    def test_new_without_self(self):
-        """ calling save without self argument """
+    def test_reload_empty(self):
+        """ Load from an empty file """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
 
-        with self.assertRaises(TypeError) as excpt:
-            FileStorage.new()
-        excpt_msg = "new() missing 2 required positional arguments:"
-        excpt_msg += " 'self' and 'obj'"
-        self.assertEqual(str(excpt.exception), excpt_msg)
+    def test_reload_from_nonexistent(self):
+        """ Nothing happens if file does not exist """
+        self.assertEqual(storage.reload(), None)
 
-    def test_new_with_args(self):
-        """ calling save with some args """
+    def test_base_model_save(self):
+        """ BaseModel save method calls storage save """
+        new = BaseModel()
+        new.save()
+        self.assertTrue(os.path.exists('file.json'))
 
-        my_model = BaseModel()
-        with self.assertRaises(TypeError) as excpt:
-            storage.new(my_model, 'junk')
-        excpt_msg = "new() takes 2 positional arguments but 3 were given"
-        self.assertEqual(str(excpt.exception), excpt_msg)
+    def test_type_path(self):
+        """ Confirm __file_path is string """
+        self.assertEqual(type(storage._FileStorage__file_path), str)
 
-    def test_new_noramlly(self):
-        """ run new with noraml args """
+    def test_type_objects(self):
+        """ Confirm __objects is a dict """
+        self.assertEqual(type(storage.all()), dict)
 
-        my_base = BaseModel()
-        setattr(storage, '_FileStorage__objects', {})
-        storage.new(my_base)
-        my_objects = getattr(storage, '_FileStorage__objects')
-        my_key = "BaseModel." + my_base.id
-        self.assertEqual(my_objects[my_key], my_base)
+    def test_key_format(self):
+        """ Key is properly formatted """
+        new = BaseModel()
+        _id = new.to_dict()['id']
+        for key in storage.all().keys():
+            temp = key
+        self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_storage_var_created(self):
+        """ FileStorage object storage created """
+        from models.engine.file_storage import FileStorage
+        print(type(storage))
+        self.assertEqual(type(storage), FileStorage)
